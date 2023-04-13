3ac23f3cd1d0251ef25858376a02ec3092820ae9
Zheng <aheze@getfind.app>
2023-04-12 23:17:59 -0700

    Add assets

--- Example/SwipeActionsExample/DebugView.swift
+++ Example/SwipeActionsExample/DebugView.swift
@@ -47,79 +47,94 @@ struct DebugView: View {
             }
         }
         .overlay {
-            VStack {
+            VStack(spacing: 12) {
                 if showingCustomizationClear {
-                    SwipeView {
-                        HStack {
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text("SwipeActions")
-                                    .font(.largeTitle)
-                                    .fontWeight(.bold)
-                                
-                                Text("Swipe for options")
-                                    .multilineTextAlignment(.leading)
-                                    .font(.system(.title2, design: .monospaced))
-                            }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                         
-                            Image(systemName: "moon.fill")
-                                .foregroundStyle(.secondary)
-                                .font(.largeTitle)
-                        }
-                        .padding(.horizontal, 36)
-                        .padding(.vertical, 32)
-                        .background(.thinMaterial)
-                        .mask(
-                            RoundedRectangle(cornerRadius: 32, style: .continuous)
-                        )
-                    } leadingActions: { _ in
-                    } trailingActions: { _ in
-                        SwipeAction {
-                            withAnimation(.spring(response: 0.8, dampingFraction: 0.4, blendDuration: 1)) {
-                                hue = true
-                            }
-                        } label: { highlight in
-                            Text("Options")
-                                .font(.title3)
-                                .opacity(0.75)
-                        } background: { highlight in
-                            VisualEffectView(.systemThinMaterial)
-                                .brightness(highlight ? -0.1 : 0)
-                        }
-                        
-                        SwipeAction {
-                            withAnimation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 1)) {
-                                showingCustomizationClear = false
-                            }
-                            
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
-                                withAnimation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 1)) {
-                                    showingCustomizationClear = true
-                                }
-                            }
-                            
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                withAnimation(.spring(response: 2.5, dampingFraction: 1, blendDuration: 1)) {
-                                    bright = true
-                                }
-                            }
-                            
-                        } label: { highlight in
-                            Text("Clear")
-                                .font(.title3)
-                                .opacity(0.75)
-                        } background: { highlight in
-                            VisualEffectView(.systemThinMaterial)
-                                .brightness(highlight ? -0.1 : 0)
-                        }
-                        .swipeActionEdgeStyling()
-                    }
-                    .swipeReadyToTriggerPadding(60)
-                    .swipeActionWidth(140)
-                    .swipeToTriggerTrailingEdge(true)
+                    view
                 }
+                view
+                view
             }
             .padding(.horizontal, 20)
         }
+    }
+    
+    var view: some View {
+        SwipeView {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("SwipeActions")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    Text("Swipe for options")
+                        .multilineTextAlignment(.leading)
+                        .font(.system(.body, design: .monospaced))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+             
+                Image(systemName: "moon.fill")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+            }
+            .padding(.horizontal, 24)
+            .padding(.vertical, 24)
+            .background(.thinMaterial)
+            .mask(
+                RoundedRectangle(cornerRadius: 32, style: .continuous)
+            )
+        } leadingActions: { _ in
+            SwipeAction {
+                withAnimation(.spring(response: 0.8, dampingFraction: 0.4, blendDuration: 1)) {
+                    hue = true
+                }
+            } label: { highlight in
+                Image(systemName: "square.and.arrow.up")
+                    .font(.title3)
+                    .opacity(0.75)
+            } background: { highlight in
+                VisualEffectView(.systemThinMaterial)
+                    .brightness(highlight ? -0.1 : 0)
+            }
+        } trailingActions: { _ in
+            SwipeAction {
+                withAnimation(.spring(response: 0.8, dampingFraction: 0.4, blendDuration: 1)) {
+                    hue = true
+                }
+            } label: { highlight in
+                Text("Options")
+                    .opacity(0.75)
+            } background: { highlight in
+                VisualEffectView(.systemThinMaterial)
+                    .brightness(highlight ? -0.1 : 0)
+            }
+            
+            SwipeAction {
+                withAnimation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 1)) {
+                    showingCustomizationClear = false
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+                    withAnimation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 1)) {
+                        showingCustomizationClear = true
+                    }
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    withAnimation(.spring(response: 2.5, dampingFraction: 1, blendDuration: 1)) {
+                        bright = true
+                    }
+                }
+                
+            } label: { highlight in
+                Text("Clear")
+                    .opacity(0.75)
+            } background: { highlight in
+                VisualEffectView(.systemThinMaterial)
+                    .brightness(highlight ? -0.1 : 0)
+            }
+            .swipeActionEdgeStyling()
+        }
+        .swipeToTriggerTrailingEdge(true)
+        .swipeActionWidth(120)
     }
 }
