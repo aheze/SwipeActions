53fe799256174beb5d27ab47392ed930d9e98ad3
Zheng <aheze@getfind.app>
2023-04-12 20:44:56 -0700

    Add license

--- Sources/SwipeActions.swift
+++ Sources/SwipeActions.swift
@@ -1,10 +1,33 @@
-//
-//  SwipeActions.swift
-//  SwipeActions
-//
-//  Created by A. Zheng (github.com/aheze) on 4/12/23.
-//  Copyright © 2023 A. Zheng. All rights reserved.
-//
+/*
+
+ SwipeActions.swift
+ SwipeActions
+
+ Created by A. Zheng (github.com/aheze) on 4/12/23.
+ Copyright © 2023 A. Zheng. All rights reserved.
+
+ MIT License
+
+ Copyright (c) 2023 A. Zheng
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import SwiftUI
 
@@ -1031,7 +1054,7 @@ public struct SwipeDeleteMaskShape: Shape {
 /// A style to remove the "press" effect on buttons.
 public struct SwipeActionButtonStyle: ButtonStyle {
     public init() {}
-    
+
     public func makeBody(configuration: Configuration) -> some View {
         return configuration.label
     }
