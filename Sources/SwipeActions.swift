6689d4d75091563ebfe7594aa01b32bc44d696da
Kevin Barnes <kevin@donniebarnes.com>
2023-05-27 17:30:07 -0400

    Put View+If in view extension

--- Sources/SwipeActions.swift
+++ Sources/SwipeActions.swift
@@ -1448,15 +1448,17 @@ struct AllowSwipeToTriggerKey: PreferenceKey {
 // MARK: Extensions
 
 
-/// Applies the given transform if the given condition evaluates to `true`.
-/// - Parameters:
-///   - condition: The condition to evaluate.
-///   - transform: The transform to apply to the source `View`.
-/// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-@ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
-    if condition {
-        transform(self)
-    } else {
-        self
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
     }
 }
